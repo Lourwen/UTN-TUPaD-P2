f20c179f7be8a613cb848bbeb08e55b2baecb594
Lourdes <lourdes.rosapaco@mail.com>
2025-08-12 23:09:43 -0300

    Entrega semana1

---
+++ 01 Introducción a Java/HolaMundo/src/holamundo/ejercicio06.java
@@ -0,0 +1,11 @@
+
+package holamundo;
+
+public class ejercicio06 {
+    public static void main(String[] args){
+     /*6. Escribe un programa que muestre el siguiente mensaje en consola:Nombre: Juan Pérez Edad: 30 años
+    Dirección: "Calle Falsa 123"       */
+        System.out.println("Nombre: Juan Perez\nEdad:30 anos\n Direccion: \"Calle Falsa 123\" ");
+        
+    }
+}
---
+++ 01 Introducción a Java/HolaMundo/src/holamundo/ejercicio10.java
@@ -0,0 +1,16 @@
+
+package holamundo;
+
+
+public class ejercicio10 {
+    
+public static void main(String[] args) { 
+    int a = 5;
+    int b = 2;
+    int resultado = a / b;
+    System.out.println("Resultado: " + resultado);
+    
+    }
+
+
+}
